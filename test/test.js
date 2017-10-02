f2d3a84e4adce998d166e91603f3c139ba5644ce
Padbras <cspecq1@Salle110>
2017-10-02 11:40:07 +0200

    Implémentation du test invalide de l'histoire deux

--- test/test.js
+++ test/test.js
@@ -6,3 +6,10 @@ LyngkTestCase.prototype.testHistoireUn = function(){
     var coord = new Lyngk.Coordinates("A", 1);
     assertTrue(coord.valide() == false);
 };
+
+
+LyngkTestCase.prototype.testHistoireDeux = function(){
+    var coord = new Lyngk.Coordinates("A", 1);
+    var somme = coord.sommeCoord();
+    assertTrue(somme  == 43);
+};
