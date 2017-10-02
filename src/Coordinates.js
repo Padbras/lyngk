8d7a9cdcbc944f14516c63d0ced9be65b01df0f8
Padbras <cspecq1@Salle110>
2017-10-02 11:31:38 +0200

    Implémentation de la fonction de test de position répondant au test unitaire de l'histoire une

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,15 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+
+    var private_c = c;
+    var private_l = l;
+
+    var coordValides = [[3,3],[2,5],[1,7],[2,7],[2,8],[3,8],[3,9],[5,8],[7,7]];
+
+    this.valide = function()
+    {
+        return (coordValides[c.charCodeAt(0) - 'A'.charCodeAt(0)][0] < l && l < coordValides[c.charCodeAt(0) - 'A'.charCodeAt(0)][1]);
+    };
+
 };
