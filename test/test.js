8f2e23d70b030cd1c0bfeb1b65beda4b163d6cee
Padbras <cspecq1@Salle110>
2017-10-02 10:37:55 +0200

    Implémentation du test invalide de l'histoire un

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
-'use strict';
+"use strict";
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testHistoireUn = function(){
+    var coord = new Lyngk.Coordinates("A", 1);
+    assertTrue(coord.valide() == false);
+};
