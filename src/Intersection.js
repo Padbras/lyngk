73b66fbbc84877e40f16975f4b801dd0c590597a
Padbras <cspecq1@Salle110>
2017-10-09 11:01:33 +0200

    Implementation du code de l'histoire huit

--- src/Intersection.js
+++ src/Intersection.js
@@ -5,10 +5,22 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 Lyngk.Intersection = function () {
 
    var private_etat = Lyngk.State.VACANT;
+   var private_color;
 
     this.get_etat = function()
     {
         return private_etat;
+    }
+
+    this.get_color = function()
+    {
+        return private_color;
+    }
+
+    this.placerPion = function(couleur)
+    {
+        private_color = couleur;
+        private_etat = Lyngk.State.ONE_PIECE;
     }
 
 };
