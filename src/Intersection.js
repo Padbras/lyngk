ea87d3ff296caba9044732f8c6a17fb5edade290
Padbras <cspecq1@Salle110>
2017-10-09 10:45:15 +0200

    Implementation du code répondant au test de l'histoire sept

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,5 +2,13 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function () {
+
+   var private_etat = Lyngk.State.VACANT;
+
+    this.get_etat = function()
+    {
+        return private_etat;
+    }
+
 };
